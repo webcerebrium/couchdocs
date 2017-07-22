bfd4b1bb9b899323edd145b4f65ac4611a2840e6
wcrbrm <viclopata@gmail.com>
2017-07-22 13:47:08 +0300

    autoupdate operation, remote tarball support

--- README.md
+++ README.md
@@ -8,52 +8,71 @@ npm install -g couchdocs
 
 ## Usage
 
-#### Create CouchDB Database from all merged JSON documents in current folder
+#### (c) Create CouchDB Database from all merged JSON documents in current folder
 ```
 couchdocs create
+couchdocs create http://example.com/database.tar.gz
 ```
 
-#### Delete existing and re-create CouchDB database from all merged documents in current folder
+#### (r) Delete existing and re-create CouchDB database from all merged documents in current folder
 ```
 couchdocs recreate
+couchdocs recreate http://example.com/database.tar.gz
 ```
 
-#### Update Database document using local json file
+#### (u) Update Database document using local json file
 ```
 couchdocs update foo.json
 couchdocs update --id _design/backend design_backend.json
 ```
 
-### Save database into JSON files
+### (a) Partial database autoupdate
+
+Read section in couchdocs file, it can declare what exact types or what documents can be safely autoupdated.
+`[autoupdate]` section can contain types that should be auto updated.
+`[exclude]` section can contain documents that should be auto updated.
+
+```
+couchdocs autoupdate
+couchdocs autoupdate  http://example.com/database.tar.gz
+```
+
+Typical Example of couchdocs sections for configuring autoupdates
+```
+[autoupdate]
+_id=_design/servers _design/public _design/auth
+type=template layout widget webapp-config
+
+[exclude]
+_id=User-Backup
+```
+
+### (s) Save database into JSON files
 
 Each document becomes a separate file
 ```
 couchdocs save
 ```
 
-## .couchdocs File Example
+### (z) Create tarball from JSON files in current folders
+
+```
+couchdocs zip
+```
+
+## .couchdocs File Configuration Example
 Contains properties of CouchDB connection which should be applied for sync of JSON files in the current folder.
 ```
 [couchdb]
 db=testdb
 url=http://localhost:5984/
 ```
-
-You could use `COUCH_URL` and `COUCH_DB` variables too for database specification.
+You could also use `COUCH_URL` and `COUCH_DB` variables too for database specification.
 
 Use `-v` to have a verbose output about what is going on.
 
+## Reference to other handy CLI utilities
 
-## coming soon:
+- [https://github.com/zaach/jsonlint]
+- [https://github.com/glynnbird/couchbackup]
 
-Read section in couchdocs file can declared what exact types or files can be safely autoupdated
-```
-couchdocs autoupdate
-```
-`[types]` can contain types that should be auto updated
-`[docs]` can contain documents that should be auto updated
-
-Verify JSON files in the current folder
-```
-couchdocs verify 
-```	
--- bin.js
+++ bin.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-
+const tmp = require('tmp');
 const parseArgs = require('minimist');
 const update = require('./lib/update');
 const database = require('./lib/database');
@@ -10,13 +10,46 @@ const save = require('./lib/save');
 const autoupdate = require('./lib/autoupdate');
 const verify = require('./lib/verify');
 const zip = require('./lib/zip');
+const shell = require('shelljs');
+
+const commandArgs = parseArgs(process.argv, { '--': true });
+const isVerbose = () => (commandArgs.v || commandArgs.verbose);
+const out = (...a) => { if (isVerbose()) console.info(...a); };
+
+let localPath = './';
+const getLocalPath = (args) => {
+  
+  return new Promise((resolve, reject) => {
+    const url = args._[3];
+    if (!url || ! /^https?:/.test(url)) { resolve(localPath); return; }
+    out('Remote URL= ', url);
+    if (!/\.tar\.gz$/.test(url)) { reject('ERROR: only TAR.GZ is expected in remote URL'); return; }
+
+    const tmpobj = tmp.dirSync();
+    localPath = tmpobj.name;
+    out('Temporary Directory: ', tmpobj.name);
+    shell.exec('cd ' + localPath + ' && curl -s ' + url + ' | tar x' + (isVerbose() ? 'v':'') + 'z');
+
+    resolve(localPath);
+  });
+};
+
+const cleanup = () => {
+  if (localPath  && localPath !== './') {
+    out('Cleaning Path=', localPath);
+    shell.exec( 'rm -rf ' + localPath );
+  }
+};
 
 const err = (e) => {
   console.log(e);
+  cleanup();
   process.exit(1);
 };
+
 const done = (data) => {
   if (typeof data === 'string') console.log(data);
+  cleanup();
   process.exit(0);
 };
 
@@ -24,6 +57,7 @@ if (process.mainModule && process.mainModule.filename === __filename) {
    const args = parseArgs(process.argv, { '--': true });
    const couchCredentials = require('./lib/credentials')(args);
    const command = args._[2];
+
    if (command === 'show') {
       console.log(couchCredentials);
 
@@ -35,33 +69,42 @@ if (process.mainModule && process.mainModule.filename === __filename) {
         })
 	.catch(err);
 
-   } else if (command === 'verify' || command === 'e') {
-
-      verify('./').then(done).catch(e);
-
    } else if (command === 'zip' || command === 'z') {
 
       verify('./')
 	.then((files)=> { zip('./').then(done).catch(err); })
 	.catch(err);
 
+   } else if (command === 'verify' || command === 'e') {
+
+      getLocalPath(args).then(path => {
+        verify(path).then(done).catch(e);
+      }).catch(err);
+
    } else if (command === 'create' || command === 'c') {
-      merge('./')
-        .then(data => { 
+
+      getLocalPath(args).then(path => {
+        merge(path).then(data => { 
            const db = database(couchCredentials);
-	   db.create().then(() => (upload(data))).then(done).catch(err); 
-	})
-        .catch(err);
+           db.create().then(() => (upload(data))).then(done).catch(err); 
+	}).catch(err);
+      }).catch(err);
 
    } else if (command === 'recreate' || command === 'r') {
-      merge('./')
-	.then(data => { 
+
+      getLocalPath(args).then(path => {
+        merge(path).then(data => { 
 	   const db = database(couchCredentials);
 	   db.removeIfExists()
              .then(() => (db.create().then(() => (db.upload(data).then(done).catch(err))).then(done).catch(err)))
              .catch(err); 
-	})
-        .catch(err);
+	}).catch(err);
+      }).catch(err);
+
+   } else if (command === 'autoupdate' || command === 'a') {
+      getLocalPath(args).then(path => {
+        autoupdate(couchCredentials, args).then(done).catch(err);
+      }).catch(err);
 
    } else if (command === 'save' || command === 's') {
       save(couchCredentials, args).then(done).catch(err);
@@ -69,11 +112,8 @@ if (process.mainModule && process.mainModule.filename === __filename) {
    } else if (command === 'update' || command === 'u') {
       update(couchCredentials, args).then(done).catch(err);
 
-   } else if (command === 'autoupdate' || command === 'a') {
-      autoupdate(couchCredentials, args).then(done).catch(err);
-
    } else {
-      err( [ "USAGE: couchdocs create|update|save|update|autoupdate" ].join('\n'));
+      err( [ "USAGE: couchdocs create|update|save|update|autoupdate|verify|merge" ].join('\n'));
    }
 
 } else {
